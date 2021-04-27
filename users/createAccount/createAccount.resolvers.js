f92a8423f53e49b5da6dcd27b01b8cef2e036d8c
Gyeonghun Park <gp112795@gmail.com>
2021-04-26 23:54:28 -0400

    Clean Code

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import client from "../client";
+import client from "../../client";
 
 export default {
   Mutation: {
@@ -22,7 +21,7 @@ export default {
           },
         });
         if (existingUser) {
-          throw new Error("This username/email is already taken.");
+          throw new Error("This username/password is already taken.");
         }
         const uglyPassword = await bcrypt.hash(password, 10);
         return client.user.create({
@@ -37,28 +36,6 @@ export default {
       } catch (e) {
         return e;
       }
-    },
-
-    login: async (_, { username, password }) => {
-      const user = await client.user.findFirst({ where: { username } });
-      if (!user) {
-        return {
-          ok: false,
-          error: "User not found.",
-        };
-      }
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: "Incorrect password.",
-        };
-      }
-      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token,
-      };
     },
   },
 };
---
+++ users/editProfile/editProfile.resolvers.js
@@ -0,0 +1,39 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Mutation: {
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password },
+        { loggedInUser }
+      ) => {
+        const uglyPassword = password && (await bcrypt.hash(password, 10));
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile.",
+          };
+        }
+      }
+    ),
+  },
+};
