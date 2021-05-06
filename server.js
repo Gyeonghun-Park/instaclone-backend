f9f5ea52c80045928cfa5bd225af2bcf52d2bd2c
Gyeonghun Park <gp112795@gmail.com>
2021-05-06 15:29:47 -0400

    S3 Photo Upload part Two

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import express from "express";
 import logger from "morgan";
+import { graphqlUploadExpress } from "graphql-upload";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
+  uploads: false,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
@@ -18,6 +20,7 @@ const apollo = new ApolloServer({
 });
 
 const app = express();
+app.use(graphqlUploadExpress());
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
--- shared/shared.utils.js
+++ shared/shared.utils.js
@@ -8,12 +8,14 @@ AWS.config.update({
 });
 
 export const uploadToS3 = async (file, userId, folderName) => {
-  const { filename, createReadStream } = await file;
+  const {
+    file: { filename, createReadStream },
+  } = await file;
   const readStream = createReadStream();
   const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
   const { Location } = await new AWS.S3()
     .upload({
-      Bucket: "instaclone-uploads",
+      Bucket: "instaclone-upload-s3",
       Key: objectName,
       ACL: "public-read",
       Body: readStream,
